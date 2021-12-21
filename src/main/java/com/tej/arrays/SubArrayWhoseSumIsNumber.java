6f6be3a8a040b8d9f5554b47b684f41fb20cfa88
Tejaswini Vanama <tejaswini.vanama@gmail.com>
2021-12-20 19:21:11 -0800

    null checks

--- src/main/java/com/tej/arrays/SubArrayWhoseSumIsNumber.java
+++ src/main/java/com/tej/arrays/SubArrayWhoseSumIsNumber.java
@@ -9,18 +9,20 @@ public class SubArrayWhoseSumIsNumber {
 		int[] res=findSubArrayOfSumValue(new int[] {42, 15, 12, 8, 6, 32},26);
 		System.out.println(Arrays.toString(res));
 	}
-	
+
 	private static int[] findSubArrayOfSumValue(int[] a,int sum) {
-		
-		
+
+		if(a==null || a.length==0) {
+			throw new IllegalArgumentException();
+		}
 		for(int i=0;i<a.length;i++) {
 			int actualSum=0;
 			for(int j=i;j<a.length;j++) {
-				
-					actualSum=actualSum+a[j];
-					if(actualSum==sum) {
-						System.out.println(Arrays.toString(Arrays.copyOfRange(a, i, j+1)));
-					}
+
+				actualSum=actualSum+a[j];
+				if(actualSum==sum) {
+					System.out.println(Arrays.toString(Arrays.copyOfRange(a, i, j+1)));
+				}
 			}
 		}
 		return new int[] {};
